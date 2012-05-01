d364f9dd32f0c197021b8346e84c05183d6fd750
ringmaster <a_github@midnightcircus.com>
2012-05-01 15:28:44 -0400

    First Commit

---
+++ review.php
@@ -0,0 +1,34 @@
+<article id="post-<?php echo $content->id; ?>" class="post review">
+	<div class="postcontrols">
+	<?php if($post->get_access(User::identify())->edit): ?>
+	<a class="edit_post" href="<?php URL::out('admin', array('page'=>'publish', 'id'=>$content->id)); ?>">Edit Review</a>
+	<?php endif; ?>
+	</div>
+
+	<header>
+		<h2>Book Review</h2>
+		<h1><a href="/az<?php echo $content->asin; ?>"><?php echo $content->title_out; ?></a></h1>
+		<p>Rating: <?php echo $content->rating; ?></p>
+	</header>
+
+	<?php echo $content->content_out; ?>
+
+	<?php if($request->display_entry): ?>
+	<section class="comments" itemprop="comment">
+		<h1 id="comments">Comments</h1>
+		<?php if($content->comments->moderated->count == 0): ?>
+			<p><?php _e('There are no comments on this post.'); ?>
+		<?php else: ?>
+			<?php foreach($content->comments->moderated->comments as $comment): ?>
+				<?php echo $theme->content($comment); ?>
+			<?php endforeach; ?>
+		<?php endif; ?>
+		<?php if($post->info->comments_disabled): ?>
+			<p><?php _e('Sorry, commenting on this post is disabled.'); ?>
+		<?php else: ?>
+		<?php $post->comment_form()->out(); ?>
+		<?php endif; ?>
+	</section>
+	<?php endif; ?>
+
+</article>
---
+++ review.plugin.php
@@ -0,0 +1,95 @@
+<?php
+
+class ReviewPlugin extends Plugin
+{
+	public function action_plugin_activation( $plugin_file )
+	{
+		Post::add_new_type( 'review' );
+	}
+
+	public function action_plugin_deactivation( $plugin_file )
+	{
+		Post::deactivate_post_type( 'review' );
+	}
+
+	public function filter_post_type_display($type, $foruse) 
+	{ 
+		$names = array( 
+			'review' => array(
+				'singular' => _t( 'Review', 'review' ),
+				'plural' => _t( 'Reviews', 'review' ),
+			)
+		); 
+		return isset($names[$type][$foruse]) ? $names[$type][$foruse] : $type; 
+	}
+
+	public function action_init()
+	{
+		$this->add_template('review', dirname($this->get_file()) . '/review.php');
+	}
+
+	public function action_form_publish_review( $form, $post )
+	{
+		$ratings = array(
+			1 => '1 / 5',
+			2 => '2 / 5',
+			3 => '3 / 5',
+			4 => '4 / 5',
+			5 => '5 / 5',
+		);
+		$form->insert('content', new FormControlSelect('rating', $post, 'Rating', $ratings, 'admincontrol_select'));
+
+		$form->insert('content', new FormControlText('asin', $post, 'ASIN', 'admincontrol_text'));
+	}
+
+	public function filter_post_rating($rating, $post)
+	{
+		if(intval($post->info->rating) != 0) {
+			$rating = $post->info->rating;
+		}
+		return $rating;
+	}
+
+	public function filter_post_asin($asin, $post)
+	{
+		if($post->info->asin != '') {
+			$asin = $post->info->asin;
+		}
+		return $asin;
+	}
+
+	public function filter_rewrite_rules( $rules )
+	{
+		$rules[] = new RewriteRule( array(
+			'name' => 'display_podcasts',
+			'parse_regex' => '%^reviews(?:/page/(?P<page>\d+))?/?$%i',
+			'build_str' => 'reviews(/page/{$page})',
+			'handler' => 'PluginHandler',
+			'action' => 'display_reviews',
+			'priority' => 7,
+			'is_active' => 1,
+			'description' => 'Displays multiple reviews',
+		));
+
+		return $rules;
+	}
+
+	public function action_plugin_act_display_reviews( $handler )
+	{
+		$paramarray['fallback'] = array(
+			'review.multiple',
+			'entry.multiple',
+			'multiple',
+			'home',
+		);
+
+		$default_filters = array(
+			'content_type' => Post::type( 'review' ),
+		);
+		$paramarray['user_filters'] = $default_filters;
+
+		return $handler->theme->act_display( $paramarray );
+	}
+}
+
+?>
